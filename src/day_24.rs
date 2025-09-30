b3da865daffb59952bfd0a6549597347b91ed600
Markus Jarderot <mizardx@gmail.com>
2025-09-30 11:07:05 +0200

    Day 24 - Refactor & tests

--- src/day_24.rs
+++ src/day_24.rs
@@ -1,5 +1,3 @@
-use std::fmt::{Display, Write};
-
 use thiserror::Error;
 
 use crate::utils::{Grid, GridParseError, TilePath, permute};
@@ -31,15 +29,6 @@ impl TryFrom<u8> for Tile {
     }
 }
 
-impl Display for Tile {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        match *self {
-            Self::Wall => f.write_char('#'),
-            Self::Open => f.write_char('.'),
-            Self::Target(n) => f.write_char((b'0' + n) as char),
-        }
-    }
-}
 impl TilePath for Tile {
     fn is_passable(&self) -> bool {
         !matches!(self, Self::Wall)
@@ -53,67 +42,107 @@ fn parse(input: &str) -> Result<Grid<Tile>, GridParseError<TileParseError>> {
 
 #[aoc(day24, part1)]
 fn part_1(grid: &Grid<Tile>) -> usize {
-    let locations = (b'0'..=b'9')
-        .map(|ch| Tile::Target(ch - b'0'))
-        .filter_map(|t| grid.find_pos(|&t1| t1 == t))
-        .collect::<Vec<_>>();
-    let distances: Vec<Vec<_>> = locations
-        .iter()
-        .map(|l1| {
-            locations
-                .iter()
-                .map(|l2| (l1 != l2).then(|| grid.shortest_path(*l1, *l2)).flatten())
-                .collect()
-        })
-        .collect();
-    let mut remaining = (1..locations.len()).collect::<Vec<_>>();
-    let mut min_distance = usize::MAX;
-    permute(&mut remaining, &mut |perm: &[usize]| {
-        let total = perm
-            .iter()
-            .fold((0, Some(0)), |(source, dist), &target| {
-                (
-                    target,
-                    dist.and_then(|d1| distances[source][target].map(|d2| d1 + d2)),
-                )
-            })
-            .1;
-        if let Some(dist) = total {
-            min_distance = min_distance.min(dist);
-        }
-    });
-    min_distance
+    find_shortest_distance(grid, false)
 }
 
 #[aoc(day24, part2)]
 fn part_2(grid: &Grid<Tile>) -> usize {
+    find_shortest_distance(grid, true)
+}
+
+fn find_shortest_distance(grid: &Grid<Tile>, close_path: bool) -> usize {
     let locations = (b'0'..=b'9')
         .map(|ch| Tile::Target(ch - b'0'))
         .filter_map(|t| grid.find_pos(|&t1| t1 == t))
         .collect::<Vec<_>>();
+
     let distances: Vec<Vec<_>> = locations
         .iter()
-        .map(|l1| {
+        .map(|&l1| {
             locations
                 .iter()
-                .map(|l2| (l1 != l2).then(|| grid.shortest_path(*l1, *l2)).flatten())
+                .map(|&l2| {
+                    if l1 == l2 {
+                        None
+                    } else {
+                        grid.shortest_path(l1, l2)
+                    }
+                })
                 .collect()
         })
         .collect();
+
     let mut remaining = (1..locations.len()).collect::<Vec<_>>();
     let mut min_distance = usize::MAX;
-    permute(&mut remaining, &mut |perm: &[usize]| {
-        let total = perm.iter().fold((0, Some(0)), |(source, dist), &target| {
-            (
-                target,
-                dist.and_then(|d1| distances[source][target].map(|d2| d1 + d2)),
-            )
-        });
-        if let (last, Some(dist)) = total
-            && let Some(closing) = distances[last][0]
-        {
-            min_distance = min_distance.min(dist + closing);
+    permute(&mut remaining, &mut |sequence: &[usize]| {
+        let mut dist = 0;
+        let mut prev = 0;
+        for &next in sequence {
+            let Some(step) = distances[prev][next] else {
+                return;
+            };
+            dist += step;
+            prev = next;
         }
+        if close_path {
+            let Some(close_dist) = distances[prev][0] else {
+                return;
+            };
+            dist += close_dist;
+        }
+        min_distance = min_distance.min(dist);
     });
     min_distance
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    const EXAMPLE: &str = "\
+        ###########\n\
+        #0.1.....2#\n\
+        #.#######.#\n\
+        #4.......3#\n\
+        ###########\n\
+    "
+    .trim_ascii();
+
+    #[test]
+    fn test_parse() {
+        const WW: Tile = Tile::Wall;
+        const OO: Tile = Tile::Open;
+        const T0: Tile = Tile::Target(0);
+        const T1: Tile = Tile::Target(1);
+        const T2: Tile = Tile::Target(2);
+        const T3: Tile = Tile::Target(3);
+        const T4: Tile = Tile::Target(4);
+        let grid = parse(EXAMPLE).unwrap();
+        let exptected = [
+            [WW; 11],
+            [WW, T0, OO, T1, OO, OO, OO, OO, OO, T2, WW],
+            [WW, OO, WW, WW, WW, WW, WW, WW, WW, OO, WW],
+            [WW, T4, OO, OO, OO, OO, OO, OO, OO, T3, WW],
+            [WW; 11],
+        ];
+        for (r, row) in exptected.into_iter().enumerate() {
+            for (c, cell) in row.into_iter().enumerate() {
+                assert_eq!(grid[(r, c)], cell);
+            }
+        }
+    }
+
+    #[test]
+    fn test_part_1() {
+        let grid = parse(EXAMPLE).unwrap();
+        let result = part_1(&grid);
+        assert_eq!(result, 14);
+    }
+
+    #[test]
+    fn test_part_2() {
+        let grid = parse(EXAMPLE).unwrap();
+        let result = part_2(&grid);
+        assert_eq!(result, 20);
+    }
 }
