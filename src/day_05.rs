10f37e1d2231f60aab4df26768dbf024395dd3db
Markus Jarderot <mizardx@gmail.com>
2025-10-14 15:20:01 +0200

    Day 5 - Parallelize with Rayon

--- src/day_05.rs
+++ src/day_05.rs
@@ -1,64 +1,104 @@
 use std::fmt::Write;
 
+use rayon::iter::{
+    IndexedParallelIterator, IntoParallelIterator, ParallelExtend, ParallelIterator,
+};
+
 const HEX: &[u8] = b"0123456789abcdef";
 
 #[aoc(day5, part1)]
 fn part_1(input: &[u8]) -> String {
-    let mut res = String::new();
+    let mut res = Vec::new();
 
-    let mut md5 = md5::Context::new();
-    md5.consume(input);
+    let root = &{
+        let mut root = md5::Context::new();
+        root.consume(input);
+        root
+    };
 
-    let mut buf = String::new();
-    for x in 1.. {
-        buf.clear();
-        write!(&mut buf, "{x}").unwrap();
+    let mut start = 1_usize;
+    let mut window_size = 256;
+    while res.len() < 8 {
+        res.par_extend(
+            (start..start + window_size)
+                .into_par_iter()
+                .map_init(String::new, |buf, x| {
+                    let mut child = root.clone();
 
-        let mut md5_step = md5.clone();
-        md5_step.consume(buf.as_bytes());
-        let hash = md5_step.finalize().0;
+                    buf.clear();
+                    write!(buf, "{x}").unwrap();
+                    child.consume(buf.as_bytes());
 
-        if let [0, 0, b, ..] = hash
-            && b >> 4 == 0
-        {
-            res.push(HEX[(b & 0xF) as usize] as char);
-            if res.len() >= 8 {
-                return res;
-            }
-        }
+                    let hash = child.finalize().0;
+
+                    if let [0, 0, b, ..] = hash
+                        && b >> 4 == 0
+                    {
+                        Some(HEX[(b & 0xF) as usize])
+                    } else {
+                        None
+                    }
+                })
+                .flatten_iter(),
+        );
+        start += window_size;
+        window_size *= 2;
     }
-    unreachable!()
+    res.truncate(8);
+    String::from_utf8(res).unwrap()
 }
 
 #[aoc(day5, part2)]
 fn part_2(input: &[u8]) -> String {
     let mut res = [0u8; 8];
 
-    let mut root = md5::Context::new();
-    root.consume(input);
+    let root = &{
+        let mut root = md5::Context::new();
+        root.consume(input);
+        root
+    };
 
     let mut mask = 0_u8;
-    let mut buf = String::new();
-    for x in 1.. {
-        buf.clear();
-        write!(&mut buf, "{x}").unwrap();
+    let mut start = 1_usize;
+    let mut window_size = 0x10000;
+    let mut events = Vec::new();
+    while mask != 0xFF {
+        events.par_extend(
+            (start..start + window_size)
+                .into_par_iter()
+                .by_exponential_blocks()
+                .map_init(String::new, |buf, x| {
+                    let mut child = root.clone();
 
-        let mut child = root.clone();
-        child.consume(buf.as_bytes());
-        let hash = child.finalize().0;
+                    buf.clear();
+                    write!(buf, "{x}").unwrap();
+                    child.consume(buf.as_bytes());
 
-        if let [0, 0, pos, ch, ..] = hash
-            && pos < 8
-            && mask & (1 << pos) == 0
-        {
-            res[pos as usize] = HEX[(ch >> 4) as usize];
-            mask |= 1 << pos;
-            if mask == 0xFF {
-                return unsafe { String::from_utf8_unchecked(res.into()) };
+                    let hash = child.finalize().0;
+
+                    if let [0, 0, pos, ch, ..] = hash
+                        && pos < 8
+                        && mask & (1 << pos) == 0
+                    {
+                        Some((pos as usize, HEX[(ch >> 4) as usize]))
+                    } else {
+                        None
+                    }
+                })
+                .flatten_iter(),
+        );
+        for &(pos, ch) in &events {
+            let bit = 1 << pos;
+            if mask & bit == 0 {
+                mask |= bit;
+                res[pos] = ch;
             }
         }
+        events.clear();
+        start += window_size;
+        window_size *= 2;
     }
-    unreachable!()
+    str::from_utf8(&res).unwrap().to_string()
 }
 
 #[cfg(test)]
