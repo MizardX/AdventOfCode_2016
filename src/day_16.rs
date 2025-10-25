80eedd5df4059b7364f46e7869ead8c6bbdb8d62
MizardX <mizardx@gmail.com>
2025-10-25 11:15:14 +0200

    Day 16 - Optimizations

--- src/day_16.rs
+++ src/day_16.rs
@@ -10,21 +10,60 @@ impl DragonCurve {
         // 2. Curve sequence
         // The root value and the inverted root value repeat every (2n+2) characters.
         // Every (n+1)'th character is from the dragon curve sequence.
-        let len = self.root.len();
-        let index1 = index % (2 * len + 2);
-        if index1 < len {
-            self.root[index1]
-        } else if index1 > len && index1 <= 2 * len {
-            !self.root[2 * len - index1]
+        let root_len = self.root.len();
+        let root_index = index % (2 * root_len + 2);
+        if root_index < root_len {
+            self.root[root_index]
+        } else if root_index > root_len && root_index <= 2 * root_len {
+            !self.root[2 * root_len - root_index]
         } else {
-            // Index into the dragon curve sequence
-            let n = (index + 1) / (len + 1);
-            // Get the bit above the lowest 1-bit of the index. This is equivalent to the dragon curve sequence.
-            // ref: https://rosettacode.org/wiki/Dragon_curve
-            let least_significant_bit = n & (!n + 1);
-            let above_mask = least_significant_bit * 2;
-            n & above_mask != 0
+            let n = (index + 1) / (root_len + 1);
+            Self::dragon_curve(n)
         }
+    }
+
+    /// Index into the dragon curve sequence
+    #[inline]
+    const fn dragon_curve(n: usize) -> bool {
+        // Get the bit above the lowest 1-bit of the index. This is equivalent to the dragon curve sequence.
+        // ref: https://rosettacode.org/wiki/Dragon_curve
+        (n >> (n.trailing_zeros() + 1)) & 1 == 1
+    }
+
+    fn get_range_xnor(&self, start: usize, end: usize) -> bool {
+        let len = self.root.len();
+        let cycle_len = 2 * len + 2;
+        let cycles_start = start.next_multiple_of(cycle_len);
+        let cycles_end = end.next_multiple_of(cycle_len).saturating_sub(cycle_len);
+        let mut result = true;
+        if cycles_end <= cycles_start {
+            // No full cycle within the range, fall back to single samples
+            for ix in start..end {
+                result ^= !self.get(ix);
+            }
+            return result;
+        }
+        for ix in start..cycles_start {
+            result ^= !self.get(ix);
+        }
+
+        // Within each full cycle, the root and inverted root will almost
+        // cancel eachother out.
+        // It is just the parity of it's length that affects the result.
+        // Of course, the dragon curve padding still matters
+        let dragon_start = (cycles_start + len + 1) / (len + 1);
+        let dragon_count = (cycles_end - cycles_start) / (len + 1); // even
+        if dragon_count & 2 == 2 && len & 1 == 1 {
+            result ^= true;
+        }
+        for ix in dragon_start..dragon_start + dragon_count {
+            result ^= !Self::dragon_curve(ix);
+        }
+
+        for ix in cycles_end..end {
+            result ^= !self.get(ix);
+        }
+        result
     }
 }
 
@@ -45,15 +84,10 @@ fn part_2(curve: &DragonCurve) -> String {
 }
 
 fn checksum(curve: &DragonCurve, disk_len: usize) -> String {
-    let chunk_size = disk_len & (!disk_len + 1); // lowest bit = maximum power-of-two factor
+    let chunk_size = 1 << disk_len.trailing_zeros();
     let mut checksum = String::new();
     for i in (0..disk_len).step_by(chunk_size) {
-        // Each char of the checksum can be calculated in in sequence within each chunk, instead of by halving.
-        // ((a xnor b) xnor (c xnor d)) == (((a xnor b) xnor c) xnor d)
-        let mut value = true; // Starting with `true`, since `(true xnor a) == a`
-        for j in i..i + chunk_size {
-            value ^= !curve.get(j);
-        }
+        let value = curve.get_range_xnor(i, i + chunk_size);
         checksum.push(if value { '1' } else { '0' });
     }
     checksum
