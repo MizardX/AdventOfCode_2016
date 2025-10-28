a7fedbcfdb7148c0ca6d37efea315db9f4fbc069
MizardX <mizardx@gmail.com>
2025-10-28 08:50:21 +0100

    Day 19 - Josephus problem

--- src/day_19.rs
+++ src/day_19.rs
@@ -8,7 +8,15 @@ fn parse(s: &str) -> Result<u64, ParseIntError> {
 
 #[expect(clippy::trivially_copy_pass_by_ref, reason = "AOC library")]
 #[aoc(day19, part1)]
-fn part_1(&count: &u64) -> u64 {
+const fn part_1_jospehus(&count: &u64) -> u64 {
+    let highest_one = 1 << (63 - count.leading_zeros());
+    (count - highest_one) * 2 + 1
+}
+
+#[expect(clippy::trivially_copy_pass_by_ref, reason = "AOC library")]
+// #[aoc(day19, part1)]
+#[allow(unused, reason = "reference, tests")]
+fn part_1_deques(&count: &u64) -> u64 {
     let mut queue = (1..=count).collect::<VecDeque<_>>();
     while let Some(first) = queue.pop_front() {
         if let Some(_second) = queue.pop_front() {
@@ -22,7 +30,20 @@ fn part_1(&count: &u64) -> u64 {
 
 #[expect(clippy::trivially_copy_pass_by_ref, reason = "AOC library")]
 #[aoc(day19, part2)]
-fn part_2(&count: &u64) -> u64 {
+const fn part_2_josephus(&count: &u64) -> u64 {
+    let mut highest_power_of_3 = 1_u64;
+    while let Some(next) = highest_power_of_3.checked_mul(3)
+        && next < count
+    {
+        highest_power_of_3 = next;
+    }
+    count.saturating_sub(highest_power_of_3) + count.saturating_sub(2 * highest_power_of_3)
+}
+
+#[expect(clippy::trivially_copy_pass_by_ref, reason = "AOC library")]
+//#[aoc(day19, part2)]
+#[allow(unused, reason = "reference, tests")]
+fn part_2_deques(&count: &u64) -> u64 {
     let mut ahead = (1..=count).collect::<VecDeque<_>>();
     let mut behind = ahead.split_off(ahead.len() / 2);
     while let Some(first) = ahead.pop_front() {
@@ -45,15 +66,33 @@ mod tests {
     use super::*;
 
     #[test]
+    fn pattern_1() {
+        for n in 1..=10_000 {
+            let res = part_1_deques(&n);
+            let approx = part_1_jospehus(&n);
+            assert_eq!(res, approx);
+        }
+    }
+
+    #[test]
+    fn pattern_2() {
+        for n in 2..=10_000 {
+            let res = part_2_deques(&n);
+            let approx = part_2_josephus(&n);
+            assert_eq!(res, approx);
+        }
+    }
+
+    #[test]
     fn test_part_1() {
-        let result = part_1(&5);
+        let result = part_1_jospehus(&5);
 
         assert_eq!(result, 3);
     }
 
     #[test]
     fn test_part_2() {
-        let result = part_2(&5);
+        let result = part_2_josephus(&5);
 
         assert_eq!(result, 2);
     }
