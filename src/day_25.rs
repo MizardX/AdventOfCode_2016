c4ee56fde78146af7dcbc2a6c5f04278a15cc776
MizardX <mizardx@gmail.com>
2025-11-02 12:06:56 +0100

    Day 25 - Optimizations

--- src/day_25.rs
+++ src/day_25.rs
@@ -97,18 +97,29 @@ struct Machine<'a> {
     ip: usize,
     registers: [i64; 4],
     stopped: bool,
-    output: &'a mut Vec<i64>,
+    output: Vec<i64>,
 }
 
 impl<'a> Machine<'a> {
-    const fn new(instructions: &'a [Instruction], output: &'a mut Vec<i64>) -> Self {
+    const fn new(instructions: &'a [Instruction]) -> Self {
         Self {
             instructions,
             ip: 0,
             registers: [0; 4],
             stopped: false,
-            output,
+            output: Vec::new(),
         }
+    }
+
+    fn reset(&mut self) {
+        self.ip = 0;
+        self.registers = [0; 4];
+        self.stopped = false;
+        self.output.clear();
+    }
+
+    fn output(&self) -> &[i64] {
+        &self.output
     }
 
     const fn get_register(&self, reg: Reg) -> i64 {
@@ -167,18 +178,32 @@ impl<'a> Machine<'a> {
     }
 }
 
-#[aoc(day25, part1)]
+// #[aoc(day25, part1)]
+#[allow(unused, reason = "Alternative solution")]
 fn part_1(instructions: &[Instruction]) -> i64 {
-    let mut output = Vec::new();
+    let mut machine = Machine::new(instructions);
     for initial_value in 0.. {
-        output.clear();
-        let mut machine = Machine::new(instructions, &mut output);
+        machine.reset();
         machine.set_register(Reg::A, initial_value);
         machine.run();
 
+        let output = machine.output();
         if output.iter().zip(&output[1..]).all(|(&a, &b)| b == 1 - a) {
             return initial_value;
         }
     }
     0
+}
+
+#[aoc(day25, part1)]
+fn part_1_faster(instructions: &[Instruction]) -> i64 {
+    let mut machine = Machine::new(instructions);
+    machine.run(); // A = 0
+    // Output will be the lowest 10 bits of A + (secret number)
+    let output = machine.output();
+    // We need to match an alternating sequence of ones and zeros, which is just:
+    let target = 0b10_1010_1010;
+    let output_num = output.iter().rev().fold(0, |s, &b| (s << 1) | b);
+    // How much we need to add to get to target
+    target - output_num
 }
